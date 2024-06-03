ccae8131d8fc8fb808e5089f3f61bb5c73ceff64
GErP83 <gurrka@gmail.com>
2024-06-03 17:25:09 +0200

    text updates

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Run Swift Format
 
-This GitHub Action ensures your Swift code adheres to a consistent coding style by running a script to format your Swift files using [swift-format](https://github.com/apple/swift-format).
+This GitHub Action ensures that the Swift code adheres to a consistent coding style by running a script to check the Swift files using [swift-format](https://github.com/apple/swift-format).
 
 ## Usage
 
-Include the action in your workflow (make sure that a Swift 5.6+ toolchain is on your PATH, on macOS this should be given, but on Linux you may need to first install it e.g. using [`setup-swift`](https://github.com/fwal/setup-swift)):
+Include the action in the workflow (make sure that a Swift 5.6+ toolchain is on the PATH, on macOS, this should be given, but on Linux, it needs to be installed first, e.g., [setup-swift](https://github.com/fwal/setup-swift)).
+
+Add a .swift-format file to the repository to configure the lint action. If no file is added, the default configuration will be used.
 
 ```yaml
 - uses: feather-actions/run-swift-format@0.0.1
--- run-swift-format.sh
+++ run-swift-format.sh
@@ -13,19 +13,18 @@ fi
 if ! test -f ${SWIFT_FORMAT_JSON_PATH}; then
   CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   SWIFT_FORMAT_JSON_PATH="${CURRENT_SCRIPT_DIR}/.swift-format"
-  log "❗ There was no '.swift-format' file in the repository, so we use the one that is included with the github action."
+  log "❗ There was no ‘.swift-format’ file in the repository, so using the one included with the GitHub action."
 fi
 
 git -C "${REPO_ROOT}" ls-files -z '*.swift' \
   | grep -z -v \
   -e 'Package.swift' \
-  | xargs -0 "${SWIFTFORMAT_BIN}" "lint --strict" --parallel \
-  --configuration "${SWIFT_FORMAT_JSON_PATH}" \
+  | xargs -0 "${SWIFTFORMAT_BIN}" "lint" --strict --configuration "${SWIFT_FORMAT_JSON_PATH}" --parallel \
   && SWIFT_FORMAT_RC=$? || SWIFT_FORMAT_RC=$?
   
 if [ "${SWIFT_FORMAT_RC}" -ne 0 ]; then
-  log "❌ Running swift-format produced errors."; exit 1;
-  exit "${SWIFT_FORMAT_RC}"
+  log "❌ Running swift-format lint produced errors."
+  exit 1
 fi
 
-log "✅ Ran swift-format with no errors."
+log "✅ Ran swift-format lint with no errors."
